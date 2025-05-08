70126cefb84f83ad01053b5d74c57616e4f24f11
Gilberto Tumangday <gilberto.tumangday@mail.utoronto.ca>
2025-05-08 16:00:19 -0400

    added basic ordering frontend functionality

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBox = document.querySelector('.menuBox');
+    const bottomBox = document.querySelector('.bottomBox');
     const originalContent = menuBox.innerHTML;
+    let numItems = 0;
 
+    function updateCart(){
+        const cartIndicator = document.getElementById("cartIndicator");
+        if(cartIndicator){
+            cartIndicator.textContent = `View Order[${numItems}]`;
+        }
+    }
     function attachHoverEvents() {
         const boxes = document.querySelectorAll(".item");
         boxes.forEach(item => {item.dataset.page = 'page.html';})
@@ -37,6 +45,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 nav.style.color = "white";
             });
         });
+        const carts = document.querySelectorAll(".cart");
+        carts.forEach(item => {item.dataset.page = 'order.html';})
     }
 
     attachHoverEvents();
@@ -102,6 +112,52 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(image){
                     image.src = imageSRC;
                 }
+
+                  // Reattach back button
+                  const backBtn = document.getElementById("backButton");
+                  if (backBtn) {
+                      backBtn.addEventListener("click", () => {
+                          menuBox.innerHTML = originalContent;
+                          menuBox.style.width = "";
+                          menuBox.style.height = "";
+                          attachHoverEvents();
+
+                      });
+                  }
+                  //order button
+                  const orderBtn = document.getElementById("orderButton");
+                  const orderBtnRem = document.getElementById("orderRemoveButton");
+                  if(orderBtn){
+                    orderBtn.addEventListener("click", ()=>{
+                        numItems = numItems+1;
+                        updateCart();
+                    });
+                  }
+                  if(orderBtnRem){
+                    orderBtnRem.addEventListener("click",()=>{
+                        if(numItems > 0){
+                            numItems = numItems-1;
+                            updateCart();
+                        }
+                    })
+                  }
+              })
+              .catch(err => console.error("Failed to load page:", err));
+        }
+    });
+    bottomBox.addEventListener('click', (e)=>{
+        if(e.target.classList.contains('cart')){
+            console.log("cart clicked");
+            const page = e.target.dataset.page;
+            const originalWidth = menuBox.offsetWidth;
+            const originalHeight = menuBox.offsetHeight;
+            menuBox.style.width = `${originalWidth}px`;
+            menuBox.style.height = `${originalHeight}px`;
+            fetch(page)
+              .then(res => res.text())
+              .then(html => {
+                menuBox.innerHTML = html;
+                attachHoverEvents();
 
                   // Reattach back button
                   const backBtn = document.getElementById("backButton");
